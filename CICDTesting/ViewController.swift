7a91f0c1b55c4c5433aae3b4b6af898fad83ffc6
Jose Mena <jmena@rccl.com>
2020-02-25 15:06:29 -0500

    Initial Commit

---
+++ CICDTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDTesting
+//
+//  Created by Jose Mena on 12/6/1398 AP.
+//  Copyright © 1398 Jose Mena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
